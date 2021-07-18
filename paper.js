eb40811a290dcd1eccfc6a04b48ebf3f51c0cac5
gayatriwhitehatjr <86729402+gayatriwhitehatjr@users.noreply.github.com>
2021-07-18 19:37:51 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,12 @@
+class paper 
+{
+    varOptions={
+        isStatic:false,
+        restitution:0.3,
+        friction:0,
+        density:1.2
+    }
+    image(){
+        this.image=loadImage("paper.png")
+    }
+}
